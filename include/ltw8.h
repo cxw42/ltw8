f387d81ebf2a8fabd2ebff2cf5e7711aa5a9391a
cxw <cxwembedded@gmail.com>
2016-09-16 11:01:18 -0400

    Began converting include files

--- include/ltw8.h
+++ include/ltw8.h
@@ -1,5 +1,8 @@
 // ltw8.h: Main header for the ltw8 UTF-8 library.
 // Copyright (c) Chris White, 2016.
 
+#include "ustring.h"
+
+// Note: ff=unix because I use cygwin as my primary git interface. //
 // vi: set ts=2 sts=2 sw=2 et ai ff=unix: //
 
