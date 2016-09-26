efbb3ca5d378d5d338e6d9f5e010cbbe29d9ddc7
Chris White <chrisw@leehayes.com>
2016-09-26 08:26:00 -0400

    bulk-changes: now can specify which files to process. Pulled enums from glibmm master.

--- src/bulk-changes.sh
+++ src/bulk-changes.sh
@@ -2,19 +2,27 @@
 # bulk-changes.sh: Large-scale changes when pulling in glibmm files.
 # Copyright (c) Chris White 2016.  LGPL3.
 
-shopt -s extglob 
+shopt -s extglob
 
-sed -i.bak 's/_GLIBMM_\([A-Z_]+_H\)/LTW8_\1_/g' *.{h,hg,cc,ccg}
+if [[ "$#" -ge 1 ]] ; then
+  items=("$@")  # save args as a list, one elem per arg.  Thanks to
+                # http://stackoverflow.com/a/12711837/2877364
+                # by http://stackoverflow.com/users/418413/kojiro
+else
+  items=(!(*.bak)) # let bash expand the glob.
+fi
+
+sed -i.bak 's/_GLIBMM_\([A-Z_]+_H\)/LTW8_\1_/g' "${items[@]}"
   # header guards
-sed -i.bak 's/Glib/ltw8/g' *.{h,hg,cc,ccg}
+sed -i.bak 's/Glib/ltw8/g' "${items[@]}"
   # namespace changes
-sed -i.bak 's/\(#include <\)glibmm\//\1/g' *.{h,hg,cc,ccg}
+sed -i.bak 's/\(#include <\)glibmm\//\1/g' "${items[@]}"
   # include-path changes
-sed -i.bak 's#^\(.*glibmmconfig.*\)$#//\1#' *.{h,hg,cc,ccg}
+sed -i.bak 's#^\(.*glibmmconfig.*\)$#//\1#' "${items[@]}"
   # don't have a glibmmconfig
 
 # Local headers
-sed -i.bak 's/#include <ustring.h>/#include "ltw8_ustring.h"/' !(*.bak)
-sed -i.bak 's/#include <unicode.h>/#include "ltw8_unicode.h"/' !(*.bak)
-sed -i.bak 's/#include <stringutils.h>/#include "ltw8_stringutils.h"/' !(*.bak)
+sed -i.bak 's/#include <ustring.h>/#include "ltw8_ustring.h"/' "${items[@]}"
+sed -i.bak 's/#include <unicode.h>/#include "ltw8_unicode.h"/' "${items[@]}"
+sed -i.bak 's/#include <stringutils.h>/#include "ltw8_stringutils.h"/' "${items[@]}"
 
